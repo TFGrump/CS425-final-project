af0aac48603983c7c7e0bb23e8d25556ffc0709b
Elijah Monroe <emonroe6@gmu.edu>
2020-12-08 17:53:09 -0500

    Added Weapon and WearableItem as extensions of GameItem;

--- GameItem.java
+++ GameItem.java
@@ -1,21 +1,29 @@
 public abstract class GameItem {
 
-   String name;
-
-   private int strengthMod;
-   private int perceptionMod;
-   private int enduranceMod;
-   private int charismaMod;
-   private int intelligenceMod;
-   private int agilityMod;
-   private int luckMod;
+   //private
+   private String name;
+   private float weight;
+   private float value;
    
-   // Get respective stat modifiers
-   public int getStrMod() { return strengthMod; }
-   public int getPerMod() { return perceptionMod; }
-   public int getEndMod() { return enduranceMod; }
-   public int getChrMod() { return charismaMod; }
-   public int getIntMod() { return intelligenceMod; }
-   public int getAgiMod() { return agilityMod; }
-   public int getLucMod() { return luckMod; }
+   //protected
+   protected GameItem(String name, float weight, float value) {
+	   this.name = name;
+	   this.value = value;
+	   this.weight = weight;
+   }
+
+   //public
+   public String getName() {
+	   return this.name;
+   }
+	
+   public float getWeight() {
+	   return this.weight;
+   }
+
+   public float getValue() {
+	   return this.value;
+   }
+
+   
 }
---
+++ Weapon.java
@@ -0,0 +1,48 @@
+
+public class Weapon extends GameItem {
+
+	//private
+	private float damage;
+	private float accuracy;
+	private float firerate;
+	private int mag;
+	private int range;
+	
+	//public
+	//constructors
+	public Weapon(String name, float weight, float value, float damage, float accuracy, float firerate, int mag, int range) {
+		super(name, weight, value);
+		this.damage = damage;
+		this.accuracy = accuracy;
+		this.firerate = firerate;
+		this.mag = mag;
+		this.range = range;
+	}
+	public Weapon(String name, float damage, float accuracy, float firerate, int mag, int range) {
+		super(name, 0, 0);
+		this.damage = damage;
+		this.accuracy = accuracy;
+		this.firerate = firerate;
+		this.mag = mag;
+		this.range = range;
+	}
+	
+	//getters
+	public float getDamage() {
+		return damage;
+	}
+	public float getAccuracy() {
+		return accuracy;
+	}
+	public float getFirerate() {
+		return firerate;
+	}
+	public int getMag() {
+		return mag;
+	}
+	public int getRange() {
+		return range;
+	}	
+	
+
+}
