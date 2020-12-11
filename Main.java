332296a64ccab56dc80c356725fc2a6b94888690
Elijah Monroe <emonroe6@gmu.edu>
2020-12-10 21:46:45 -0500

    Added to the wordlists; Added Icon to startup

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws FileNotFoundException {
       setup();
       
       // TODO: Put main menu stuff here
+      
+      System.out.println("Welcome to Gangster the Game!");
+      
+      Scanner scanner = new Scanner(new File("icon/icon.txt"));
+      System.out.println(scanner.toString());
+      scanner.close();
+      
       CommandCreateWorld cmd1 = new CommandCreateWorld();
       TextInputManager.addCommand(cmd1);
 
